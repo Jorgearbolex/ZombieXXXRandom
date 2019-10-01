46a559488f8ebd458693dd515f9c6f40f1a8b420
Jorgearbolex <jorgearbolex@gmail.com>
2019-10-01 16:48:50 -0500

    ahi va ya listo taller 4
    ahi va ya listo taller 4

---
+++ zombieloquesea/zombieauxrandom/Assets/FPSim.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FPSim : MonoBehaviour
+{
+    public float rotY;
+    
+    void Start()
+    {
+
+    }
+
+    float x = 0;
+    float y = 0;
+
+    void Update()
+    {
+        if (Time.timeScale == 0) // detiene todo
+            return;
+
+        x -= Input.GetAxis("Mouse Y");
+        y += Input.GetAxis("Mouse X");
+        transform.eulerAngles = new Vector3(x, y, 0);
+        rotY = transform.eulerAngles.y;
+    }
+
+
+}
---
+++ zombieloquesea/zombieauxrandom/Assets/HeroControl.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using NPC.Enemy;
+using NPC.Ally;
+
+
+public class HeroControl : MonoBehaviour
+{
+
+    public Color col;
+    public static System.Random veloRamdHeroe = new System.Random();
+    public readonly float veloHeroe = veloRamdHeroe.Next (10, 14);
+    public GameObject finalMensaje;
+
+   GameObject empty;
+
+    void Start()
+    {
+
+       
+        GetComponent<MeshRenderer>().material.color = col;
+
+        empty = new GameObject();
+        empty.name = "Camera";
+        empty.AddComponent<Camera>();
+        empty.AddComponent<FPSim>();
+        empty.transform.SetParent(this.transform);
+        empty.transform.localPosition = Vector3.zero;
+
+        GameObject heroe = gameObject;
+
+        heroe.AddComponent<MoveHeroe>();
+        heroe.GetComponent<MoveHeroe>().myVelo = veloHeroe;
+        heroe.GetComponent<MoveHeroe>().veloCam = empty.GetComponent<FPSim>();
+
+        finalMensaje = GameObject.Find("GAME OVER");
+        finalMensaje.SetActive(false);// esto es para que le mensaje no se muestra desde el principio
+
+    }
+
+
+    
+
+    void Update()
+    {
+ 
+
+    }
+
+    
+
+    private void OnCollisionEnter(Collision col)
+    {
+        
+        if (col.gameObject.name == "podrido")
+        {
+            finalMensaje.SetActive(true);
+            Time.timeScale = 0;
+        }
+    }
+}
